707a86dd15f1510c6bae8c08a83c1dd151382427
Archy17 <1070711@gmail.com>
2018-11-26 02:09:11 +0300

    first commit

---
+++ lib/discuss/topic/top.ex
@@ -0,0 +1,20 @@
+defmodule Discuss.Topic.Top do
+  use Ecto.Schema
+  import Ecto.Changeset
+  ##alias Snappy.Accounts.{User, Encryption}
+
+
+  schema "topics" do
+    field :title, :string
+
+  end
+
+   ##def changeset(%Struct{} = struct, params) do
+   def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+    
+  end
+
+end 
---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,63 @@
+defmodule DiscussWeb.TopicController do 
+  use DiscussWeb, :controller 
+
+  alias Discuss.Topic.Top
+
+  def index(conn, _params) do
+    topics = Discuss.Repo.all(Discuss.Topic.Top)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do 
+     changeset = Top.changeset(%Top{}, %{})
+
+     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"top" => top}) do
+    changeset = Top.changeset(%Top{}, top)
+    
+   case Discuss.Repo.insert(changeset) do
+      {:ok, _topic} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+
+    end
+  end 
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Discuss.Repo.get(Top, topic_id)
+    changeset = Top.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+  
+  def update(conn, %{"id" => topic_id, "top" => top}) do
+    old_topic = Discuss.Repo.get(Top, topic_id)
+    changeset = Top.changeset(old_topic, top)
+
+    changeset = Discuss.Repo.get(Top, topic_id) |> Top.changeset(top)
+
+    case Discuss.Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset
+    end
+
+  end
+
+  def delete(conn, %{"id" => topic_id}) do 
+    Discuss.Repo.get!(Top, topic_id) |> Discuss.Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
+
+end
