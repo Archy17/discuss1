24cf28b8097434e667b3a524baf8919945966193
Archy17 <1070711@gmail.com>
2019-01-09 21:41:26 +0300

    133  :assign not found in: %Phoenix.Socket

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,15 +1,35 @@
 defmodule DiscussWeb.CommentsChannel do 
   use DiscussWeb, :channel 
+  alias Discuss.Repo
+  alias Discuss.Topic.Top
+  alias Discuss.Comment
 
-  def join(name, _params, socket) do
-    IO.puts("+++++")
-    IO.puts(name)
-    {:ok, %{hey: "There"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    #IO.puts(name)  
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Top, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, %{"content" => content}, socket) do
+  #  IO.puts("++++")
+  #  IO.puts(name)
+  #  IO.inspect(message)
+    topic = socket.assign.topic.top
 
-  end
+    changeset = topic
+      |> Ecto.build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
 
+
+      case Repo.insert(changeset) do 
+         {:ok, comment} ->
+          {:reply, :ok, socket}
+         {:error, _reason} ->
+          {:reply, {:error, %{errors: changeset}}, socket}
+      
+      end
+   end
 end
 
